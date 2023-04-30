f4091c2ef61ba2d3ac1b37ec11dd5383c57495eb
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-04-30 19:10:38 +0300

    Update Server.java

--- Server/Server.java
+++ Server/Server.java
@@ -3,6 +3,29 @@ import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 
 public class Server {
+
+    private static final int DELAY_TIME_MS = 500;
+
+    public static void IndeterminateDotProgressBar(){
+        int count = 0;
+        while (true) {
+            System.out.print(".");
+            try {
+                Thread.sleep(DELAY_TIME_MS);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            count++;
+            if (count == 10) {
+                System.out.print("\r");  // move cursor back to beginning of line
+                for (int i = 0; i < 10; i++) {
+                    System.out.print(" ");  // overwrite dots with spaces
+                }
+                System.out.print("\r");  // move cursor back to beginning of line
+                count = 0;
+            }
+        }
+    }
     public static void main(String[] args) {
         try {
             System.out.println("Starting Server");
@@ -22,6 +45,7 @@ public class Server {
             registry.rebind("l", uni1);
             registry.rebind("m", uni2);
             System.out.println("Server Has Started");
+            IndeterminateDotProgressBar();
 
         } catch (Exception e) {
             System.out.println("Server side Error" + e);
