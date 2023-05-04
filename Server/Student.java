b79552108a7f9df413eadd2eb4f326e8617e63dd
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-05-04 22:10:56 +0300

    Update Student.java

--- Server/Student.java
+++ Server/Student.java
@@ -1,11 +1,12 @@
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
-public interface Student extends Remote{
+public interface Student extends Remote {
 
     public String getName() throws RemoteException;
+
     public int getAge() throws RemoteException;
 
 }
 
-//Interface
+// Interface
