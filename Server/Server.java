14e9314ae79d7ad825fcf2148a82e476764f9243
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-06-26 21:07:49 +0300

    Update Server.java

--- Server/Server.java
+++ Server/Server.java
@@ -4,36 +4,36 @@ import java.rmi.server.UnicastRemoteObject;
 
 public class Server {
 
-    private static final int DELAY_TIME_MS = 500;
+    // private static final int DELAY_TIME_MS = 500;
 
-    public static void IndeterminateDotProgressBar() {
-        int count = 0;
-        while (true) {
-            System.out.print(".");
-            try {
-                Thread.sleep(DELAY_TIME_MS);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            count++;
-            if (count == 10) {
-                System.out.print("\r"); // move cursor back to beginning of line
-                for (int i = 0; i < 10; i++) {
-                    System.out.print(" "); // overwrite dots with spaces
-                }
-                System.out.print("\r"); // move cursor back to beginning of line
-                count = 0;
-            }
-        }
-    }
+    // public static void IndeterminateDotProgressBar() {
+    //     int count = 0;
+    //     while (true) {
+    //         System.out.print(".");
+    //         try {
+    //             Thread.sleep(DELAY_TIME_MS);
+    //         } catch (InterruptedException e) {
+    //             e.printStackTrace();
+    //         }
+    //         count++;
+    //         if (count == 10) {
+    //             System.out.print("\r"); // move cursor back to beginning of line
+    //             for (int i = 0; i < 10; i++) {
+    //                 System.out.print(" "); // overwrite dots with spaces
+    //             }
+    //             System.out.print("\r"); // move cursor back to beginning of line
+    //             count = 0;
+    //         }
+    //     }
+    // }
 
     public static void main(String[] args) {
         try {
-            System.out.println("Starting Server");
+            // System.out.println("Starting Server");
 
             System.setProperty("java.rmi.server.hostname", "127.0.0.1");
 
-            System.out.println("...........");
+            // System.out.println("...........");
 
             StudentImpl s1 = new StudentImpl("Shariff", 22);
             StudentImpl s2 = new StudentImpl("Glorry", 20);
@@ -46,7 +46,7 @@ public class Server {
             registry.rebind("l", uni1);
             registry.rebind("m", uni2);
             System.out.println("Server Has Started");
-            IndeterminateDotProgressBar();
+            // IndeterminateDotProgressBar();
 
         } catch (Exception e) {
             System.out.println("Server side Error" + e);
