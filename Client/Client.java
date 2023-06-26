022de293d275b9485f106662c3fdad1749472c45
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-06-26 21:08:36 +0300

    Update Client.java

--- Client/Client.java
+++ Client/Client.java
@@ -4,22 +4,20 @@ import java.rmi.registry.LocateRegistry;
 
 public class Client {
     public static void main(String[] args) {
-        try{
+        try {
             Registry registry = LocateRegistry.getRegistry("127.0.0.1", 9000);
 
             Student x = (Student) registry.lookup("l");
             Student y = (Student) registry.lookup("m");
-        
-            System.out.println("shariff Age : "+ x.getAge());
-            System.out.println("Shariff Name : "+ x.getName());
-            System.out.println("Glorry Name : "+ y.getName());
-            System.out.println("Glorry Age :"+ y.getAge());
 
-            
-        }catch(Exception e){
-            System.out.println("Client Side Error"+e);
+            System.out.println("shariff Age : " + x.getAge());
+            System.out.println("Shariff Name : " + x.getName());
+            System.out.println("Glorry Name : " + y.getName());
+            System.out.println("Glorry Age :" + y.getAge());
+
+        } catch (Exception e) {
+            System.out.println("Client Side Error" + e);
         }
     }
-   
-}
 
+}
